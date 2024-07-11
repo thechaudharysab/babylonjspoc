7607516f713564b6a76506a5f6e077a154e9eaba
Chaudhry Talha <talha440@gmail.com>
2024-07-11 15:28:56 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 } from 'react-native';
 
 import { useEngine, EngineView } from '@babylonjs/react-native';
-import { ArcRotateCamera, Camera, Scene, SceneLoader, Color4, Color3, HemisphericLight, Vector3, MeshBuilder, StandardMaterial, AnimationGroup, Nullable, AnimationPropertiesOverride, DirectionalLight, ShadowGenerator, Mesh } from "@babylonjs/core";
+import { ArcRotateCamera, Camera, Scene, SceneLoader, Color4, AnimationGroup, Nullable } from "@babylonjs/core";
 import '@babylonjs/loaders/glTF';
 
 // import { elevenlabs_API_KEY } from './src/constants';
@@ -21,118 +21,85 @@ function App(): React.JSX.Element {
   const droneGLTFURL = 'https://raw.githubusercontent.com/thechaudharysab/babylonjspoc/main/src/assets/drone/buster_drone.gltf';
   const glbTest = 'https://github.com/thechaudharysab/babylonjspoc/raw/main/src/assets/Client.glb';
 
+  const Client = "https://github.com/ibjects/Pailo/raw/main/src/assets/glb3D/Client.glb";
+  const DwarfIdle = "https://github.com/ibjects/Pailo/raw/main/src/assets/glb3D/DwarfIdle.glb";
+
+  const ClientGLTF = "https://raw.githubusercontent.com/thechaudharysab/babylonjspoc/main/src/assets/Client.gltf";
+
   const engine = useEngine();
   const [scene, setScene] = useState<Scene>();
   const [camera, setCamera] = useState<Camera>();
   const [currentAnimation, setCurrentAnimation] = useState<Nullable<AnimationGroup>>(null);
 
+  // const renderWalkingMan = () => {
+  //   SceneLoader.LoadAsync(dancingManGLTFURL, undefined, engine).then((loadScene) => {
+  //     if (loadScene) {
+  //       setScene(loadScene);
+  //       loadScene.createDefaultCameraOrLight(true, undefined, true);
+  //       (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
+  //       (loadScene.activeCamera as ArcRotateCamera).radius = 10;
+  //       (loadScene.activeCamera as ArcRotateCamera).pinchPrecision = 200;
+  //       setCamera(loadScene.activeCamera!);
 
-  const renderWalkingMan = () => {
-    SceneLoader.LoadAsync(dancingManGLTFURL, undefined, engine).then((loadScene) => {
-      if (loadScene) {
-        setScene(loadScene);
-        loadScene.createDefaultCameraOrLight(true, undefined, true);
-        (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
-        (loadScene.activeCamera as ArcRotateCamera).radius = 10;
-        (loadScene.activeCamera as ArcRotateCamera).pinchPrecision = 200;
-        setCamera(loadScene.activeCamera!);
+  //       loadScene.animationGroups.forEach((animationGroup) => {
+  //         console.log("Animation Name:", animationGroup.name);
+  //       });
+  //     } else {
+  //       console.error("Error loading loadScene.");
+  //     }
+  //   }).catch((error) => {
+  //     console.error("Error loading scene: ", error);
+  //   });
+  // };
 
-        loadScene.animationGroups.forEach((animationGroup) => {
-          console.log("Animation Name:", animationGroup.name);
-        });
-      } else {
-        console.error("Error loading loadScene.");
-      }
-    }).catch((error) => {
-      console.error("Error loading scene: ", error);
-    });
-  };
+  // const renderClient = () => {
+  //   SceneLoader.LoadAsync(ClientGLTF, undefined, engine).then((loadScene) => {
+  //     if (loadScene) {
+  //       setScene(loadScene);
+  //       // Light
+  //       const light = new HemisphericLight("light", new Vector3(0, 1, 0), loadScene);
+  //       light.intensity = 0.7;
 
-  const renderHorse = () => {
-    SceneLoader.LoadAsync(horseGLTFURL, undefined, engine).then((loadScene) => {
-      if (loadScene) {
-        setScene(loadScene);
-        loadScene.createDefaultCameraOrLight(true, undefined, true);
-        (loadScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
-        (loadScene.activeCamera as ArcRotateCamera).radius = 10;
-        (loadScene.activeCamera as ArcRotateCamera).pinchPrecision = 200;
-        setCamera(loadScene.activeCamera!);
+  //       // Camera
+  //       const camera = new ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2, -6, new Vector3(0, 2, 0), loadScene, true);
+  //       setCamera(camera);
+  //     } else {
+  //       console.error("Error loading loadScene.");
+  //     }
+  //   }).catch((error) => {
+  //     console.error("Error loading scene: ", error);
+  //   });
+  // };
 
-        var idleAnimation = loadScene.getAnimationGroupByName('Idle');
+  // const clyinderTest = () => {
+  //   // if (engine) {
+  //   const scene = new Scene(engine!);
 
-        if (idleAnimation) {
-          // Adding true will loop the animation
-          idleAnimation.play(true);
-        } else {
-          console.warn("Animation not found:", idleAnimation);
-        }
+  //   const camera = new ArcRotateCamera('camera', Math.PI / 2, Math.PI / 2, 5, new Vector3(0, 0, 0), scene);
+  //   camera.attachControl(true);
+  //   setCamera(camera);
 
-        // loadScene.animationGroups.forEach((animationGroup) =>
-        //   console.log("Animation Name:", animationGroup.name));
+  //   const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
+  //   light.intensity = 0.7;
 
-        // Some notes on animations
-        // const idleAnimationName = "Idle_2"
-        // const animationGroup = loadScene.animationGroups.find((group) => group.name === idleAnimationName);
-        // if (animationGroup) {
-        //   animationGroup.play();
-        // } else {
-        //   console.warn("Animation not found:", idleAnimationName);
-        // }
-        // loadScene.animationGroups.forEach((animationGroup) => {
-        //   console.log("Animation Name:", animationGroup.name);
-        //   /**
-        //    * LOG  Animation Name: Attack_Headbutt
-        //    * LOG  Animation Name: Attack_Kick
-        //    * LOG  Animation Name: Death
-        //    * LOG  Animation Name: Eating
-        //    * LOG  Animation Name: Gallop
-        //    * LOG  Animation Name: Gallop_Jump
-        //    * LOG  Animation Name: Idle
-        //    * LOG  Animation Name: Idle_2
-        //    * LOG  Animation Name: Idle_Headlow
-        //    * LOG  Animation Name: Idle_HitReact1
-        //    * LOG  Animation Name: Idle_HitReact2
-        //    * LOG  Animation Name: Jump_toIdle
-        //    * LOG  Animation Name: Walk
-        //    */
-        // });
-      } else {
-        console.error("Error loading loadScene.");
-      }
-    }).catch((error) => {
-      console.error("Error loading scene: ", error);
-    });
-  };
+  //   const cylinder = MeshBuilder.CreateCylinder('cylinder', { height: 1, diameter: 1 }, scene);
 
-  const clyinderTest = () => {
-    // if (engine) {
-    const scene = new Scene(engine!);
+  //   const shinyMaterial = new StandardMaterial('shiny', scene);
+  //   shinyMaterial.diffuseColor = new Color3(1, 0, 0); // Red color
+  //   shinyMaterial.specularColor = new Color3(1, 1, 1); // White color for shiny effect
+  //   shinyMaterial.specularPower = 64; // Increase the shine effect
 
-    const camera = new ArcRotateCamera('camera', Math.PI / 2, Math.PI / 2, 5, new Vector3(0, 0, 0), scene);
-    camera.attachControl(true);
-    setCamera(camera);
+  //   cylinder.material = shinyMaterial;
 
-    const light = new HemisphericLight('light', new Vector3(0, 1, 0), scene);
-    light.intensity = 0.7;
-
-    const cylinder = MeshBuilder.CreateCylinder('cylinder', { height: 1, diameter: 1 }, scene);
-
-    const shinyMaterial = new StandardMaterial('shiny', scene);
-    shinyMaterial.diffuseColor = new Color3(1, 0, 0); // Red color
-    shinyMaterial.specularColor = new Color3(1, 1, 1); // White color for shiny effect
-    shinyMaterial.specularPower = 64; // Increase the shine effect
-
-    cylinder.material = shinyMaterial;
-
-    setScene(scene);
-    // }
-  };
+  //   setScene(scene);
+  //   // }
+  // };
 
   //#region Tween
 
-  const [idleAnimation, setIdleAnimation] = useState<Nullable<AnimationGroup>>(null);
-  const [walkAnimation, setWalkAnimation] = useState<Nullable<AnimationGroup>>(null);
-  const [jumpAnimation, setJumpAnimation] = useState<Nullable<AnimationGroup>>(null);
+  // const [idleAnimation, setIdleAnimation] = useState<Nullable<AnimationGroup>>(null);
+  // const [walkAnimation, setWalkAnimation] = useState<Nullable<AnimationGroup>>(null);
+  // const [jumpAnimation, setJumpAnimation] = useState<Nullable<AnimationGroup>>(null);
 
   const renderHorseExperiments = () => {
     SceneLoader.LoadAsync(horseGLTFURL, undefined, engine).then((loadScene: Scene) => {
@@ -145,24 +112,7 @@ function App(): React.JSX.Element {
         arcCamera.pinchPrecision = 200;
         setCamera(arcCamera);
 
-        const light1 = new HemisphericLight("light1", new Vector3(0, 1, 0), loadScene);
-        light1.intensity = 0.6;
-        light1.specular = Color3.Black();
-
-        const light2 = new DirectionalLight("dir01", new Vector3(0, -0.5, -1.0), loadScene);
-        light2.position = new Vector3(0, 5, 5);
-
-        const shadowGenerator = new ShadowGenerator(1024, light2);
-        shadowGenerator.useBlurExponentialShadowMap = true;
-        shadowGenerator.blurKernel = 32;
-
         const idleAnim = loadScene.getAnimationGroupByName('Idle');
-        const walkAnim = loadScene.getAnimationGroupByName('Eating');
-        const jumpAnim = loadScene.getAnimationGroupByName('Walk');
-
-        if (idleAnim) setIdleAnimation(idleAnim);
-        if (walkAnim) setWalkAnimation(walkAnim);
-        if (jumpAnim) setJumpAnimation(jumpAnim);
 
         if (idleAnim) {
           idleAnim.start(true, 1.0, idleAnim.from, idleAnim.to, true);
@@ -175,97 +125,57 @@ function App(): React.JSX.Element {
     });
   }
 
-  const blendAnimations = (from: AnimationGroup, to: AnimationGroup, duration: number) => {
-    let startTime = Date.now();
-    const frame = () => {
-      let elapsed = (Date.now() - startTime) / 1000;
-      let blend = elapsed / duration;
-      if (blend > 1) blend = 1;
+  // const blendAnimations = (from: AnimationGroup, to: AnimationGroup, duration: number) => {
+  //   let startTime = Date.now();
+  //   const frame = () => {
+  //     let elapsed = (Date.now() - startTime) / 1000;
+  //     let blend = elapsed / duration;
+  //     if (blend > 1) blend = 1;
 
-      from.setWeightForAllAnimatables(1 - blend);
-      to.setWeightForAllAnimatables(blend);
+  //     from.setWeightForAllAnimatables(1 - blend);
+  //     to.setWeightForAllAnimatables(blend);
 
-      if (blend < 1) {
-        requestAnimationFrame(frame);
-      } else {
-        from.stop();
-        to.start(true);
-      }
-    };
-    requestAnimationFrame(frame);
-  };
+  //     if (blend < 1) {
+  //       requestAnimationFrame(frame);
+  //     } else {
+  //       from.stop();
+  //       to.start(true);
+  //     }
+  //   };
+  //   requestAnimationFrame(frame);
+  // };
 
-  const handleButtonPress = () => {
-    if (idleAnimation && walkAnimation && jumpAnimation) {
-      // Start blending from Idle to Walk
-      blendAnimations(idleAnimation, walkAnimation, 1);
+  // const handleButtonPress = () => {
+  //   if (idleAnimation && walkAnimation && jumpAnimation) {
+  //     // Start blending from Idle to Walk
+  //     blendAnimations(idleAnimation, walkAnimation, 1);
 
-      // After blending to Walk, blend to Jump
-      setTimeout(() => {
-        blendAnimations(walkAnimation, jumpAnimation, 1);
-      }, 3000); // Adjust the timing for walk to jump transition
+  //     // After blending to Walk, blend to Jump
+  //     setTimeout(() => {
+  //       blendAnimations(walkAnimation, jumpAnimation, 1);
+  //     }, 3000); // Adjust the timing for walk to jump transition
 
-      // After blending to Jump, blend back to Idle
-      setTimeout(() => {
-        blendAnimations(jumpAnimation, idleAnimation, 1);
-      }, 6000); // Adjust the timing for jump to idle transition
-    }
-  };
+  //     // After blending to Jump, blend back to Idle
+  //     setTimeout(() => {
+  //       blendAnimations(jumpAnimation, idleAnimation, 1);
+  //     }, 6000); // Adjust the timing for jump to idle transition
+  //   }
+  // };
 
   const renderTestCharacter = () => {
 
-    SceneLoader.LoadAsync(glbTest, undefined, engine).then((loadedScene) => {
+    SceneLoader.LoadAsync(dancingManGLTFURL, undefined, engine).then((loadedScene) => {
       if (loadedScene) {
         setScene(loadedScene);
 
         // Create a camera
         loadedScene.createDefaultCameraOrLight(true, undefined, true);
         (loadedScene.activeCamera as ArcRotateCamera).alpha += Math.PI;
-        // (loadedScene.activeCamera as ArcRotateCamera).radius = 10;
-        // (loadedScene.activeCamera as ArcRotateCamera).pinchPrecision = 200;
         setCamera(loadedScene.activeCamera!);
 
-        // Create a hemispheric light
-        const hemiLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), loadedScene);
-        hemiLight.intensity = 0.6;
-
-        // Create a directional light for shadows
-        const directionalLight = new DirectionalLight("dirLight", new Vector3(-1, -2, -1), loadedScene);
-        directionalLight.position = new Vector3(20, 40, 20);
-
-        // Create a shadow generator
-        const shadowGenerator = new ShadowGenerator(1024, directionalLight);
-        shadowGenerator.useBlurExponentialShadowMap = true;
-        shadowGenerator.blurKernel = 32;
-
-        // Primary blud background color
-        const primaryBlueColor = new Color4(208 / 255, 236 / 255, 255 / 255, 1.0);
-        loadedScene.clearColor = primaryBlueColor;
-
-        // Configure shadows for the character mesh
-        loadedScene.meshes.forEach(mesh => {
-          mesh.receiveShadows = true;
-          shadowGenerator.addShadowCaster(mesh);
-
-          // // Adjust camera to fit the character in view
-          // let min = new Vector3(Infinity, Infinity, Infinity);
-          // let max = new Vector3(-Infinity, -Infinity, -Infinity);
-
-          // loadedScene.meshes.forEach(mesh => {
-          //   if (mesh instanceof Mesh) {
-          //     const boundingInfo = mesh.getBoundingInfo();
-          //     min = Vector3.Minimize(min, boundingInfo.minimum);
-          //     max = Vector3.Maximize(max, boundingInfo.maximum);
-          //   }
-          // });
-
-          // const center = Vector3.Center(min, max);
-          // const radius = Vector3.Distance(min, max) * 1.5;
-
-          // camera.target = center;
-          // camera.radius = radius;
-
-        });
+        // Light blue background color
+        const lighBlueColor = new Color4(208 / 255, 236 / 255, 255 / 255, 1.0);
+        loadedScene.clearColor = lighBlueColor;
 
       } else {
         console.error("Error loading loadedScene.");
@@ -281,27 +191,27 @@ function App(): React.JSX.Element {
   useEffect(() => {
     if (engine) {
       renderTestCharacter();
-      // renderHorse();
+      // renderClient();
       // renderHorseExperiments();
       // renderWalkingMan();
       // clyinderTest();
     }
   }, [engine]);
 
-  // const startWalkAnimation = () => {
-  //   if (scene) {
-  //     const walkAnimation = scene.getAnimationGroupByName("Walk");
-  //     if (walkAnimation) {
-  //       if (currentAnimation) {
-  //         currentAnimation.stop();
-  //       }
-  //       walkAnimation.play(true);
-  //       setCurrentAnimation(walkAnimation);
-  //     } else {
-  //       console.warn("Animation not found:", walkAnimation);
-  //     }
-  //   }
-  // };
+  const startWalkAnimation = () => {
+    if (scene) {
+      const walkAnimation = scene.getAnimationGroupByName("Walk");
+      if (walkAnimation) {
+        if (currentAnimation) {
+          currentAnimation.stop();
+        }
+        walkAnimation.play(true);
+        setCurrentAnimation(walkAnimation);
+      } else {
+        console.warn("Animation not found:", walkAnimation);
+      }
+    }
+  };
 
   const stopWalkAnimation = () => {
     if (scene) {
@@ -344,28 +254,20 @@ function App(): React.JSX.Element {
   return (
     <View style={styles.container}>
       <EngineView camera={camera} displayFrameRate={true} />
-      <View style={styles.absoluteView}>
-        {/* <TouchableOpacity style={styles.buttonContainer} onPress={() => {
-          mode === "Idle" ? startWalkAnimation() : stopWalkAnimation();
-        }}>
-          <Text>{mode === "Idle" ? "Start" : "Stop"} Walk</Text>
-        </TouchableOpacity> */}
-        {/* <TouchableOpacity style={styles.buttonContainer} onPress={startWalkAnimation}>
+      {/* <View style={styles.absoluteView}>
+        <TouchableOpacity style={styles.buttonContainer} onPress={startWalkAnimation}>
           <Text>Start Walking</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttonContainer} onPress={stopWalkAnimation}>
           <Text>Stop Walking</Text>
-        </TouchableOpacity> */}
-        {/* <TouchableOpacity style={styles.buttonContainer} onPress={doJumpAnimation}>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonContainer} onPress={doJumpAnimation}>
           <Text>Jump</Text>
         </TouchableOpacity>
         <TouchableOpacity style={styles.buttonContainer} onPress={randomizeBGColor}>
           <Text>Random BG Color</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.buttonContainer} onPress={handleButtonPress}>
-          <Text>Start Animation</Text>
-        </TouchableOpacity> */}
-      </View>
+      </View> */}
     </View>
   );
 }
@@ -373,7 +275,7 @@ function App(): React.JSX.Element {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#282c34',
+    // backgroundColor: '#282c34',
   },
   absoluteView: {
     position: 'absolute',
